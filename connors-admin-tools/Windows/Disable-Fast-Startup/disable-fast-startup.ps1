0c7dbd9781748088923b5d254f45ee1ca2f880ee
cqhudson <36829070+cqhudson@users.noreply.github.com>
2023-03-06 08:29:07 -0600

    Add files via upload

---
+++ connors-admin-tools/Windows/Disable-Fast-Startup/disable-fast-startup.ps1
@@ -0,0 +1,16 @@
+﻿#
+# Author: Connor Hudson
+# Date: February 2023
+# Description: Script disables Fast Startup via regkey
+#
+
+$Path = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power"
+$Name = "HiberbootEnabled"
+$value = "0"
+
+If (!(Test-Path $Path)){
+    New-Item -Path $Path -Force | Out-Null
+    New-ItemProperty -Path $Path -Name $Name -Value $value -PropertyType DWORD -Force | Out-Null
+} ELSE {
+    New-ItemProperty -Path $Path -Name $Name -Value $value -PropertyType DWORD -Force | Out-Null
+}
