7ebfd0fe8440710a02377ca680582557fcd539a2
cqhudson <36829070+cqhudson@users.noreply.github.com>
2023-03-06 11:21:58 -0600

    Add files via upload

---
+++ connors-admin-tools/Windows/Delete-Local-User-Profiles/Delete-LocalUserProfiles.ps1
@@ -0,0 +1,33 @@
+﻿
+$profiles = Get-CimInstance -ComputerName $computer -Class Win32_UserProfile
+
+$NetworkService = "C:\WINDOWS\ServiceProfiles\NetworkService"
+$LocalService = "C:\WINDOWS\ServiceProfiles\LocalService"
+$System32 = "C:\WINDOWS\system32\config\systemprofile"
+
+$CurrentUserProfile = $env:USERPROFILE
+
+# Loop through each profile
+foreach ($profile in $profiles) {
+    
+    $profileName = $profile.LocalPath
+
+    # Check to make sure the selected profile is not a required Windows profile
+    if ($profileName -ne $NetworkService -or $profileName -ne $LocalService -or $profileName -ne $System32) {
+        
+        # Check if the profile is not currently loaded
+        if ($profile.Loaded -eq $false) {
+        
+            $profileName = $profile.LocalPath
+
+            Write-Host "Deleting profile: $profileName"
+
+            # Remove the profile
+            Remove-CimInstance -InputObject $profile
+        }
+
+    }
+
+
+    
+}
