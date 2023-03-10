ace779b0a3cf737f3073d09db7fb2f6b0f938218
Connor Hudson <36829070+cqhudson@users.noreply.github.com>
2023-03-10 15:54:13 -0600

    Add files via upload

---
+++ connors-admin-tools/Windows/Generate-SSH-Keys/gen-sshkeys-and-copy-to-clipboard-one-liner.ps1
@@ -0,0 +1,2 @@
+﻿if (-not(Test-Path "$env:USERPROFILE\.ssh\id_rsa.pub" -PathType Leaf) -or -not(Test-Path "$env:USERPROFILE\.ssh\id_rsa" -PathType Leaf)){echo 'y' | ssh-keygen.exe -f $env:USERPROFILE\.ssh\id_rsa -q -N '""'} type $env:USERPROFILE\.ssh\id_rsa.pub | Set-Clipboard
+
---
+++ connors-admin-tools/Windows/Generate-SSH-Keys/gen-sshkeys-and-copy-to-clipboard.ps1
@@ -0,0 +1,14 @@
+﻿
+$Private_Key = "$env:USERPROFILE\.ssh\id_rsa"
+$Public_Key = "$env:USERPROFILE\.ssh\id_rsa.pub"
+
+# Check to see if the public and private keys already exist so we dont overwrite them.
+# If either of them are missing, generate them.
+if (-not(Test-Path $Public_Key -PathType Leaf) -or -not(Test-Path $Private_Key -PathType Leaf))
+{
+    # echo 'y' 
+    echo 'y' | ssh-keygen.exe -f $env:USERPROFILE\.ssh\id_rsa -q -N '""'
+} 
+
+# Print the contents of id_rsa.pub and copy them into the clipboard
+type $env:USERPROFILE\.ssh\id_rsa.pub | Set-Clipboard
